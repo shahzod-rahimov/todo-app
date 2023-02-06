1d559bbc4e204a9068dd4e2307f849cabe4656fe
Shahzod Rahimov <rakhimovsh3235@gmail.com>
2023-02-06 12:56:59 +0500

    ok

---
+++ src/script/index.js
@@ -0,0 +1,104 @@
+"use strict";
+
+// ----------------- HTML ELEMENTS -----------------------
+
+const toast = document.querySelector(".toast");
+const notif = document.querySelector("#notif");
+const submitform = document.querySelector("#submitform");
+const taskTitle = document.querySelector("#task");
+const doneTask = document.querySelector("#done");
+const progressTask = document.querySelector("#progress");
+const mainList = document.querySelector(".list");
+const delTask = document.querySelectorAll("#del");
+const editTask = document.querySelectorAll("#edit");
+const checkTask = document.querySelectorAll("#check");
+
+// ----------------- TASK LIST -----------------------
+
+let task = [];
+
+// ----------------- DYNAMIC TASK LIST RENDERING -----------------------
+
+function renderTaskList(tasklist) {
+  mainList.innerHTML = "";
+  if (tasklist.length) {
+    tasklist.forEach((task) => {
+      let content = `
+                <p class="text-[#5a5a5a]">${task.title}</p>
+                <div class="btn-group flex justify-between">
+                  <i data-del="${
+                    task.id
+                  }" class="del bx bx-trash text-red-400 text-2xl mx-2 cursor-pointer active:text-red-500" ></i>
+                  <i data-edit="${
+                    task.id
+                  }" class="edit bx bxs-edit text-yellow-400 text-2xl mx-2 cursor-pointer active:text-yellow-500" ></i>
+                  <i data-check="${
+                    task.id
+                  }" class="check bx bx-check-circle text-2xl mx-2 cursor-pointer ${
+        task.status
+          ? "text-green-400  active:text-green-600"
+          : "text-black active:text-[#3a3a3a]"
+      }" ></i>
+                </div>`;
+
+      const taskItem = createElement(
+        "li",
+        "list__item w-full p-3 flex justify-between shadow-lg text-lg rounded-md mb-3",
+        content
+      );
+      mainList.append(taskItem);
+    });
+  } else {
+    mainList.innerHTML =
+      "<h2 class='text-center text-xl text-red-600'>NOT FOUND</h2>";
+  }
+}
+
+renderTaskList(task);
+
+// ----------------- TASK LIST PROGRESS -----------------------
+
+function countProgress() {
+  doneTask.textContent = task.filter((task) => task.status).length;
+  progressTask.textContent = task.filter((task) => !task.status).length;
+}
+
+countProgress();
+
+// ----------------- ADD NEW TASK  -----------------------
+
+function addNewTask() {
+  const title = taskTitle.value;
+  const newTask = {
+    id: Date.now(),
+    title,
+    status: false,
+  };
+
+  const check = {
+    title: newTask.title.trim().length === 0,
+  };
+
+  if (check.title) {
+    alert("Please enter a title for this task");
+  } else {
+    task.push(newTask);
+    taskTitle.value = "";
+  }
+}
+
+submitform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addNewTask();
+  renderTaskList(task);
+  countProgress();
+});
+
+mainList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
+    const id = e.target.getAttribute("data-del");
+    task = task.filter((t) => t.id != id);
+    renderTaskList(task);
+    countProgress();
+  }
+});
